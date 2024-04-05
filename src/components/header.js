011eb9e3b9f0a928958733ad2056cbe7a9361450
Caleb Holland <101285025+holland-cw3@users.noreply.github.com>
2024-04-05 17:56:19 -0400

    name lol

--- src/components/header.js
+++ src/components/header.js
@@ -7,7 +7,7 @@ import { Link } from 'react-scroll';
 
 const Header = () => {
   return <div className=''>
-    <h1 className="banner text-2xl mt-3"> Caleb Holland</h1>
+    <h1 className="banner text-2xl mt-3"> Caleb W Holland</h1>
     <button className="connect"><Link to="contact" activeClass="active2" smooth="true" spy="true" className="cursor-pointer w-[60px] h-[50px] ml-4 mr-4 mb-2 text-xl">Contact</Link></button>
   </div>;
 };
