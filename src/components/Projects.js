2f2b58edf767dcbe7b0dfa6e8d15a7270670bd2c
Caleb Holland <101285025+holland-cw3@users.noreply.github.com>
2024-04-22 11:37:42 -0400

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -27,18 +27,27 @@ const About = () => {
               <span className="italics text-white ">Object Oriented Programming & Graphics</span>
             </div> */}
           </div>
-         
-          {
+          <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
+            <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
+            <img className="group-hover:scale-125 transition-all duration-500" src={p4} alt="p1"/>
+            <div className='absolute -bottom-full left-12 group-hover:top-5 transition-all duration-500 z-50'>
+              <span className="text-3xl text-gradient2"><a href="https://github.com/UMDWebmaster/clubrunningumd/">UMD Club Running Website</a></span>
+            </div>
+            {/* <div className='absolute -bottom-full left-12 group-hover:top-14 transition-all duration-700 z-50'>
+              <span className="italics text-white ">Object Oriented Programming & Graphics</span>
+            </div> */}
+          </div>
+          {/*
           <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
             <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
             <img className="group-hover:scale-125 transition-all duration-500" src={p3} alt="p3"/>
             <div className='absolute -bottom-full left-12 group-hover:top-5 transition-all duration-500 z-50'>
-              <span className="text-3xl text-gradient2"><a href="https://cdaily.co/">CD Rates Webscraper - IP</a></span>
+              <span className="text-3xl text-gradient2"><a href="https://github.com/holland-cw3/cd_rates_webscraper">CD Rates Webscraper - IP</a></span>
             </div>
             {/* <div className='absolute -bottom-full left-12  group-hover:top-14 transition-all duration-700 z-50'>
-              <span className="italics text-white ">IP - CD Rates Webscraper</span>
-            </div>  */}
-        
+              <span className="italics text-white ">IP - CD Rates Webscraper</span> 
+            </div>
+          </div>*/}
         </div>
         <div className='flex-1 flex flex-col gap-y-10'>
           <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
